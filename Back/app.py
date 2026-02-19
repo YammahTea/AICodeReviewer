26d6fef4adea8b2836a181d56f723a1b0d0de852
clocky <cloudyqiqi@gmail.com>
2026-02-20 02:59:27 +0300

    added extra dependency and added a way to fetch the diff file content

--- Back/app.py
+++ Back/app.py
@@ -1,5 +1,5 @@
-from fastapi import FastAPI, Request
-
+from fastapi import FastAPI, Request, status, HTTPException
+import httpx
 
 app = FastAPI()
 
@@ -12,12 +12,32 @@ async def receive_github_webhook(
   payload = await request.json()
   
   if event_type == "ping":
-    zen_message = payload.get("zen")
-    print(f"github zen: {zen_message}")
     return {"status": "ping received"}
   
-  action = payload.get("action") # example: pull req
-  print(f"Action type: {action}")
-  
+  if event_type == "pull_request":
+    action = payload.get("action")
+    
+    # only review the pr when it is opened or updated (synchronize)
+    if action in ["opened", "synchronize"]:
+      pr_number = payload["pull_request"]["number"]
+      diff_url = payload["pull_request"]["diff_url"]
+      
+      print(f"pull req number: {pr_number}")
+      print(f"pull req diff url: {diff_url}")
+      
+      # to get the code changes
+      # follow_redirects or u will get 302 status code
+      # as github redirects the request
+      async with httpx.AsyncClient(follow_redirects=True) as client:
+        response = await client.get(diff_url)
+        
+      if response.status_code == 200:
+        diff_text = response.text
+        
+        print(diff_text) # first 200 chars, just for testing
+      
+      else:
+        raise HTTPException(status_code=response.status_code, detail="Something went wrong")
+      
   return {"status": "success"}
   
